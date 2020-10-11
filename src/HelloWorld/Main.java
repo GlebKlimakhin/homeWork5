aeeeb0955a16ca3781bf29ae4f18f73c65a5af5f
glebklimakhin <klimakhiny@mail.ru>
2020-10-11 23:32:31 +0300

    HW5

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -8,7 +8,7 @@ class Main {
         employees[1] = new Employee("Ivanov Nick", "Junior developer", "II@mail.ru", "890974356", 20000, 48);
         employees[2] = new Employee("Ivanov Idris", "Senior developer", "II@mail.ru", "890974356", 20000, 56);
         employees[3] = new Employee("Ivanov Yuri", "Junior developer", "II@mail.ru", "890974356", 20000, 18);
-        employees[4] = new Employee("Ivanov Oleg", "Developer", "II@mail.ru", "890974356", 20000, 41);
+        employees[4] = new Employee("Ivanov Oleg", "CEO", "II@mail.ru", "890974356", 20000, 41);
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getAge() > 40) {
                 employees[i].getInfo();
